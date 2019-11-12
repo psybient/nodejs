6eae07423f2961989211d31f70a7806cf77b9254
psynim <nima.psybient@gmail.com>
2019-11-12 14:44:59 +0330

    Init(code) : start from an answer and NodeJs docs https://stackoverflow.com/a/58815035/11070678 https://nodejs.org/dist/latest-v12.x/docs/ .. /api/esm.html#esm_package_exports

---
+++ src/index.mjs
@@ -0,0 +1,9 @@
+
+const express = require("express");
+import * as R from 'ramda'
+const src = {name:"Bob",age:7};
+const _src = [1,2,3];//R.clone(src);
+console.log(_src);
+
+//module.exports = require('./your_server_file.js')
+
